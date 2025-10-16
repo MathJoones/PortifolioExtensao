78855a9c5f3aa3ac3f49582690446cf5557989bb
KleytonL <kleytonl.vieira@gmail.com>
2025-10-16 10:44:31 -0300

    Criando novas páginas para futuras implementações

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function hideSidebar() {
     const sidebar = document.getElementById("sidebar");
     sidebar.style.display = 'none';
 }
+
+window.onscroll = function() {
+    const goBackBtn = document.getElementById("goBackBtn")
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        goBackBtn.style.display = 'block';
+    } else {
+        goBackBtn.style.display = 'none';
+    }
+}
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
