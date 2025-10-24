f59f6f8d7cbfb8d4415f6ff5bd39ab41235ad887
Kleyton Luiz <kleytonl.vieira@gmail.com>
2025-10-23 22:02:21 -0300

    Criando script para slides

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const slides = document.querySelectorAll('.slides li');
+let slideIndex = 0;
+
 function showSidebar() {
     const sidebar = document.getElementById("sidebar");
     sidebar.style.display = 'flex';
@@ -20,4 +23,39 @@ window.onscroll = function() {
 function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener("DOMContentLoaded", initSlide);
+
+function initSlide() {
+    if(slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+    }
+}
+
+function showSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    initSlide();
+}
+
+function nextSlide() {
+    slideIndex++;
+
+    if(slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    showSlide();
+}
+
+function prevSlide() {
+    slideIndex--;
+
+    if(slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    
+    showSlide(slideIndex);
 }
